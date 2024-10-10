b068be990c145d42950db6bc4bf7cf4b02285bb9
Bjørn Moholt <bjorn@moholt.dev>
2024-10-10 21:39:09 +0200

    More cleanup and some comments

--- src/main.zig
+++ src/main.zig
@@ -16,17 +16,6 @@ fn feed() void {
     peripherals.RTC_CNTL.SWD_WPROTECT.SWD_WKEY = 0x0;
 }
 
-fn disableWatchdogs() void {
-    // Disable WDT Write protection
-    peripherals.RTC_CNTL.WDTWPROTECT.WDT_WKEY = 0x50D83AA1;
-    peripherals.RTC_CNTL.SWD_WPROTECT.SWD_WKEY = 0x8F1D312A;
-
-    // Disable watchdogs
-    peripherals.TIMG0.WDTCONFIG0.WDT_EN = 0;
-    peripherals.RTC_CNTL.WDTCONFIG0.WDT_EN = 0;
-    peripherals.RTC_CNTL.SWD_CONF.SWD_DISABLE = 1;
-}
-
 const sections = struct {
     extern var _data_start: u8;
     extern var _data_end: u8;
@@ -61,21 +50,35 @@ export fn _start() linksection("_flash_start") callconv(.C) noreturn {
         : [eos] "r" (0x3fc7ffff),
     );
     meminit();
+    main();
+}
 
+fn main() noreturn {
+    // ROM Bootloader writes to UART and I can see it on my machine, so we verify that TXFIFO_CNT has stuff to write still
+    if (peripherals.UART0.STATUS.TXFIFO_CNT > 0) {
+        //peripherals.RTC_CNTL.OPTIONS0.SW_SYS_RST = 1; // This resets chip if it's left in, proving that TXFIFI_CNT has stuff in it. It is reset before writing all the normal startup stuff
+    }
+
+    // Wait untill buffer is empty so we can check it later
     while (peripherals.UART0.STATUS.TXFIFO_CNT > 0) {}
 
     const buffer = "Hello esp32";
 
-    for (buffer) |char| {
-        //while (peripherals.UART0.STATUS.TXFIFO_CNT > 8) {}
-        peripherals.UART0.FIFO.RXFIFO_RD_BYTE = char;
-    }
-
+    // We loop here just in case it would make a difference
     while (true) {
-        if (peripherals.UART0.STATUS.TXFIFO_CNT > 0) {
-            peripherals.UART0.FIFO.RXFIFO_RD_BYTE = sections._bss_end;
-            peripherals.RTC_CNTL.OPTIONS0.SW_SYS_RST = 1;
+        for (buffer) |char| {
+            //while (peripherals.UART0.STATUS.TXFIFO_CNT > 8) {}
+            peripherals.UART0.FIFO.RXFIFO_RD_BYTE = char;
         }
-        feed();
+
+        var i: usize = 0;
+        while (i < 10000) {
+            // This resets the chip if we manage to write to UART buffer.
+            if (peripherals.UART0.STATUS.TXFIFO_CNT > 0) {
+                peripherals.RTC_CNTL.OPTIONS0.SW_SYS_RST = 1; // This does not trigger, proving we are not writing to UART buffer
+            }
+            feed();
+            i += 1;
+        }
     }
 }
